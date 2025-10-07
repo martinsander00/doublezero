492ca995dda59860e58f8d26d41b6c43a4b7162e
martinsander00 <msanderh@gmail.com>
2025-10-06 22:55:22 -0500

    Validate public IP doesn't clash with dz_prefix
    Resolves: #1836

--- smartcontract/programs/doublezero-serviceability/src/error.rs
+++ smartcontract/programs/doublezero-serviceability/src/error.rs
@@ -113,6 +113,8 @@ pub enum DoubleZeroError {
     AccessPassNotFound, // variant 53
     #[error("User account not found")]
     UserAccountNotFound, // variant 54
+    #[error("Invalid Public IP: IP conflicts with DZ prefix")]
+    InvalidPublicIp, // variant 55
 }
 
 impl From<DoubleZeroError> for ProgramError {
@@ -173,6 +175,7 @@ impl From<DoubleZeroError> for ProgramError {
             DoubleZeroError::InvalidAccountOwner => ProgramError::Custom(52),
             DoubleZeroError::AccessPassNotFound => ProgramError::Custom(53),
             DoubleZeroError::UserAccountNotFound => ProgramError::Custom(54),
+            DoubleZeroError::InvalidPublicIp => ProgramError::Custom(55),
         }
     }
 }
@@ -234,6 +237,7 @@ impl From<u32> for DoubleZeroError {
             52 => DoubleZeroError::InvalidAccountOwner,
             53 => DoubleZeroError::AccessPassNotFound,
             54 => DoubleZeroError::UserAccountNotFound,
+            55 => DoubleZeroError::InvalidPublicIp,
 
             _ => DoubleZeroError::Custom(e),
         }
@@ -313,6 +317,10 @@ mod tests {
             InvalidVlanId,
             InvalidMaxBandwidth,
             InvalidMulticastIp,
+            InvalidAccountOwner,
+            AccessPassNotFound,
+            UserAccountNotFound,
+            InvalidPublicIp,
         ];
         for err in variants {
             let pe: ProgramError = err.clone().into();
